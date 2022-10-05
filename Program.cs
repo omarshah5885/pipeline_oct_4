849e96c872b6655555a04526d3a2d56c9ec51495
Omar Shah <umarshah0@gmail.com>
2022-10-04 22:37:55 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+using Console = Colorful.Console;
+
+namespace pipeline_oct_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteAscii("Hello World!", Color.Red);
+        }
+    }
+}
